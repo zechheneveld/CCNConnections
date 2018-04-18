1529158a5b6f67667f2d9345792e310a17c3645e
Jacob Taylor <jacobtaylor@Jacobs-MacBook-Pro.local>
2018-04-18 09:48:23 -0700

    update

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bootbox = require('bootbox');
 
 mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
