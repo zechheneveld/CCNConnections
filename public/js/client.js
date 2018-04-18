fd262065dbf983282a285c99b347c6be14dc1f77
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2018-04-18 10:22:14 -0700

    add one number and send message working

--- public/js/client.js
+++ public/js/client.js
@@ -1,39 +1,55 @@
 console.log("Client loaded");
+var addedNumbers = [];
 
 $(document).ready(function () {
+    init();
+});
+
+function init() {
+
 
     $("#btnAdd").on("click", function (e) {
         e.preventDefault();
 
-           var addedNumbers = {
-             number: "1"+$("#txtClient1").val()
-         };
+        addedNumbers.push("1" + $("#txtClient1").val());
+
+        $("#txtClient1").val("");
+    });
+
+    $("#messageSend").on("click", function (e) {
+        e.preventDefault();
+
+        var SentOver = {
+            number: addedNumbers,
+            message: $("#form_message").val()
+        };
         $("#txtClient1").val("");
 
         $.ajax({
             type: "POST",
             url: "/ccn",
-            data: addedNumbers,
-            success: function(responseFromServer){
+            data: SentOver,
+            success: function (responseFromServer) {
                 console.log(responseFromServer);
             }
         });
-        console.log(addedNumbers)
+        console.log(SentOver)
     });
     // $("#messageSend").on("click", function (e) {
     //     e.preventDefault();
-    //
-    //     var messages = {
-    //         message: $("#form_message").val()
-    //     };
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "/ccn",
-    //         data: messages,
-    //         success: function(Message){
-    //             console.log(Message);
-    //         }
-    //     });
-    //     console.log(messages)
+
+        // var messages = {
+        //     message: $("#form_message").val()
+        // };
+        // $.ajax({
+        //     type: "POST",
+        //     url: "/ccn",
+        //     data: messages,
+        //     success: function(Message){
+        //         console.log(Message);
+        //     }
+        // });
+        // console.log(messages)
     // });
-});
+
+}
--- routes/ccn.js
+++ routes/ccn.js
@@ -10,17 +10,19 @@ var addedNumbers = [];
 var message;
 
 // //Message
-// router.post('/', function (req, res, next) {
-//     console.log(req.body);
-//     res.send({messageFromServer: 'Got message!'});
-//     message = req.body.message;
-
-//Numbers
 router.post('/', function (req, res, next) {
     console.log(req.body);
-    res.send({messageFromServer: 'Got number!'});
+    res.send({messageFromServer: 'Got message!'});
+    message = req.body.message;
     addedNumbers.push(req.body.number);
-    console.log(addedNumbers);
+    console.log(req.body.number);
+
+//Numbers
+// router.post('/', function (req, res, next) {
+//     console.log(req.body);
+//     res.send({messageFromServer: 'Got number!'});
+//     addedNumbers.push(req.body.number);
+//     console.log(addedNumbers);
 
 
 
@@ -30,7 +32,7 @@ const twilio = require('twilio')(
     process.env.TWILIO_AUTH_TOKEN,
     process.env.TWILIO_MESSAGING_SERVICE_SID
 );
-const body = 'Welcome to CCN Connections';
+const body = 'both working';
 
 
 
@@ -46,7 +48,7 @@ const body = 'Welcome to CCN Connections';
             return twilio.messages.create({
                 to: numbers,
                 from: process.env.TWILIO_MESSAGING_SERVICE_SID,
-                body: body
+                body: message
             });
         })
     )
